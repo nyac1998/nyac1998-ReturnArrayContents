7341fadb655d066ace28b7a62550c64426a9d087
Nya Currie <nya1998@gmail.com>
2023-02-26 22:09:17 +0000

    return array contents

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -29,7 +29,11 @@ public class Lab {
      * @param nums the array to be manipulated.
      * @return a string representation of all values of nums, with the values separated by spaces.
      */
-    public String arrayToString(int[] nums){
-        return "";
+        public String arrayToString(int[] nums){
+            String str = "";
+            for(int i = 0; i < nums.length; i++){
+                str += nums[i] + " ";
+                } 
+            return str;
     }
 }
